f2e2d4e6eb94d59af795d702729009ae7de6d5cc
Kampa Sanjay <kampasanjay856@gmail.com>
2026-04-23 11:50:38 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -2,3 +2,10 @@ import pymysql
 conn = pymysql.connect(host='localhost', user='Sanjay', password='root')
 
 print("connected")
+
+cur = conn.cursor()
+cur.execute('use day1')
+cur.execute('select * from schooling')
+tables=cur.fetchall()
+for x in tables:
+    print(x)
